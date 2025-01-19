28ea867f66a696ba980b7bb463d94516a7ed1ea3
TheOnlyFidji <119022043+TheOnlyFidji@users.noreply.github.com>
2025-01-19 19:19:00 +0100

    Update provis.sh

--- provis.sh
+++ provis.sh
@@ -38,6 +38,7 @@ CHECKPOINT_MODELS=(
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
     "https://civitai.com/api/download/models/870960"
+    "https://civitai.com/api/download/models/1199750"
 )
 
 LORA_MODELS=(
